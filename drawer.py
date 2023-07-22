fcd113154a1c823526527f10dbd2f09d4a5e8ac5
Victor Wu <victorwu313@gmail.com>
2023-07-21 23:32:01 -0400

    define main and tarot drawing interface

---
+++ drawer.py
@@ -0,0 +1,14 @@
+import generator
+
+def main():
+    # for now stick to print
+    # adding GUI later
+    print("drawing ... ")
+    card = generator.drawCard()
+
+    print(card)
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ generator.py
@@ -0,0 +1,34 @@
+import random
+
+# upper bound of drawing a major arcana
+majorArcanaMax = 22
+
+# list of all available cards
+majorArcana = [
+    "The Fool",
+    "The Magician",
+    "The High Priestess",
+    "The Empress",
+    "The Emperor",
+    "The Hierophant",
+    "The Lovers",
+    "The Chariot",
+    "Strength",
+    "The Hermit",
+    "Wheel of Fortune",
+    "Justice",
+    "The Hanged Man",
+    "Death",
+    "Temperance",
+    "The Devil",
+    "The Tower",
+    "The Star",
+    "The Moon",
+    "The Sun",
+    "Judgement",
+    "The World"
+]
+
+def drawCard():
+    cardNum = random.randint(0, majorArcanaMax)
+    return majorArcana[cardNum]
